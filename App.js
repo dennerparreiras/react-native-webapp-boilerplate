f90932af0466748ef83c5c47ced8cd1fb19173bc
Denner Parreiras <dennerparreiras@gmail.com>
2024-03-15 15:34:32 -0300

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Styled from 'styled-components/native';
+
+const StyledText = Styled(Text)`
+  color: blue;
+  background-color: yellow;
+`;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world! Test!</Text>
+      <StyledText>Hello styled world!</StyledText>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
